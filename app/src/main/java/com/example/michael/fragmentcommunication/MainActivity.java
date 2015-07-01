4c35471bf32669278a42d3643e939b3b8cee42ce
Michael Jordan <michael.jordan_desenv@hotmail.com>
2015-07-01 18:05:07 -0300

    Finalizando exercício

--- app/src/main/java/com/example/michael/fragmentcommunication/MainActivity.java
+++ app/src/main/java/com/example/michael/fragmentcommunication/MainActivity.java
@@ -6,33 +6,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements TextFragment.OnInvertListener {
+
+    private ResultFragment resultFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultFrag = (ResultFragment) getFragmentManager().findFragmentById(R.id.frag_result);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onInvert(String text) {
+        resultFrag.invert(text);
     }
 }
